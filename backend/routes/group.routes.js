e30a904d5b81fe202736c8cae257c3cf9b1c9c8d
bilal1718 <muhbil171845@gmail.com>
2024-03-02 16:19:03 +0500

    Group chat model and routes

---
+++ backend/routes/group.routes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  addMemberToGroup,
+  getGroupDetails,
+  createGroup,
+  getGroupMembers,
+  getGroupMessages,
+  removeMemberFromGroup,
+  sendMessageToGroup
+} from "../controllers/group.controller.js";
+
+const router = express.Router();
+router.post('/create', createGroup);
+router.get('/:id', getGroupDetails);
+router.post('/:id/members/add', addMemberToGroup);
+router.post('/:id/members/remove', removeMemberFromGroup);
+router.get('/:id/members', getGroupMembers);
+router.post('/:groupId/messages/send/:userId', sendMessageToGroup);
+router.get('/:id/messages', getGroupMessages);
+
+export default router;
