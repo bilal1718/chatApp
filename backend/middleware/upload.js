0f32284bf0006c59694ff587edba40e743f3390c
bilal1718 <muhbil171845@gmail.com>
2024-03-02 14:49:52 +0500

    Middle ware logic for file uploading

---
+++ backend/middleware/upload.js
@@ -0,0 +1,45 @@
+import multer from 'multer';
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'upload');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const protectImageRoute = async (req, res, next) => {
+    try {
+		const token = req.cookies.jwt;
+
+		if (!token) {
+			return res.status(401).json({ error: "Unauthorized - No Token Provided" });
+		}
+
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+		if (!decoded) {
+			return res.status(401).json({ error: "Unauthorized - Invalid Token" });
+		}
+
+		const user = await User.findById(decoded.userId).select("-password");
+
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
+
+		req.user = user;
+
+		next();
+	} catch (error) {
+		console.log("Error in protectRoute middleware: ", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};
+
+export { upload, protectImageRoute };
