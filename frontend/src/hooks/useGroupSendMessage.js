e11d9f328dc0a2f85c6e87bdeee17ec6fa1292ec
bilal1718 <muhbil171845@gmail.com>
2024-03-03 21:28:59 +0500

    Hooks for sending messages and getting group details

---
+++ frontend/src/hooks/useGroupSendMessage.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import useGroups from "../zustand/useGroups";
+import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
+import axios from "axios";
+
+const useGroupSendMessage = () => {
+	const [loading, setLoading] = useState(false);
+	const { groupMessages, setGroupMessages, selectedGroup } = useGroups();
+    const { authUser } = useAuthContext();
+
+	const sendGroupMessage = async (message) => {
+		setLoading(true);
+		try {
+			const response = await axios.post(
+				`/api/groups/${selectedGroup._id}/messages/send/${authUser._id}`, 
+				{
+					text: message,
+				},
+				{
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				}
+			);
+			const data = response.data;
+
+			if (data.error) {
+				throw new Error(data.error);
+			}
+
+			setGroupMessages((prevGroupMessages) => {
+				return [...prevGroupMessages, data];
+			  });
+		} catch (error) {
+			toast.error(error.message);
+			console.error(error);
+			console.log(message);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	return { sendGroupMessage, loading };
+};
+
+export default useGroupSendMessage;
