2905c1f3127fe7c3e13d0c0678ea632e0e3dc127
bilal1718 <muhbil171845@gmail.com>
2024-03-06 12:34:09 +0500

    Move styling files path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import "./App.css";
+import "./styles/App.css";
 import Home from "./pages/home/Home";
+import Home2 from "./pages/home/Home2";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
 import { Toaster } from "react-hot-toast";
@@ -11,7 +12,8 @@ function App() {
 	return (
 		<div className='p-4 h-screen flex items-center justify-center'>
 			<Routes>
-				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+				<Route path='/' element={authUser ? <Home2 /> : <Navigate to={"/login"} />} />
+				<Route path='/groups' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
 				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
 				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
 			</Routes>
